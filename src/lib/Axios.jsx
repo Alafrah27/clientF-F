caf7a60e08076f03d0124df1a5f01ba78446357d
[ali edris] <[almusdarthafa999@gmail.com]>
2024-12-05 10:32:21 -0800

    second commit

--- src/lib/Axios.jsx
+++ src/lib/Axios.jsx
@@ -3,4 +3,6 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: "https://flowerandfirewood.onrender.com/api/v1",
   withCredentials: true,
+  // "https://flowerandfirewood.onrender.com/api/v1"
+  // "http://localhost:3000/api/v1"
 });
